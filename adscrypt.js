15c654ae280008a127efa4557266486e56147fd2
zarzoneieir <143227542+zarzoneieir@users.noreply.github.com>
2024-12-22 13:30:58 +0300

    гзвфеу

---
+++ adscrypt.js
@@ -0,0 +1,3 @@
+document.getElementById('cta-button').addEventListener('click', function() {
+    alert('Спасибо за ваш интерес! Пожалуйста, посетите наш сайт для получения дополнительной информации.');
+});
